8cc5de5c4be12a801e4fc35ea9a34dde9f5450c3
Yasir <code.yasir@gmail.com>
2022-04-19 06:38:16 +0300

    add sidebar logic & context data

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,45 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  const { isSideBarOpen, toggleSideBar } = useGlobalContext();
+  return (
+    <aside className={`${isSideBarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" className="logo" />
+        <button className="close-btn" onClick={toggleSideBar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
 
-export default Sidebar
+      <ul className="social-icons">
+        {social.map((item) => {
+          const { id, url, icon } = item;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
