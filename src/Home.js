da5bf2f09c36fbf8f367141674bb6ffb7a0102ad
Yasir <code.yasir@gmail.com>
2022-04-19 06:38:43 +0300

    add home component logic & context data

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,19 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Home = () => {
-  return <h2>home component</h2>
-}
+  const { toggleSideBar, toggleModal } = useGlobalContext();
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={toggleSideBar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={toggleModal}>
+        show modal
+      </button>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
